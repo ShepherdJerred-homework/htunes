3007421695b62369a8a00c743b47e5df35613c5c
Jerred Shepherd <ShepherdJerred@gmail.com>
2017-10-17 15:33:20 -0500

    Populate ListBox with songs

--- htunes/MainWindow.xaml.cs
+++ htunes/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,26 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using hTunes;
 
 namespace htunes {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window {
+    public partial class MainWindow : Window
+    {
+
+        private MusicLib musicLib;
+
         public MainWindow() {
             InitializeComponent();
+            musicLib = new MusicLib();
+            setupListBox();
+        }
+
+        public void setupListBox()
+        {
+            songList.ItemsSource = musicLib.Songs.AsEnumerable();
         }
     }
 }
--- htunes/MusicLib.cs
+++ htunes/MusicLib.cs
@@ -6,11 +6,10 @@ using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Data;
+using htunes;
 
-namespace hTunes
-{
-    class MusicLib
-    {
+namespace hTunes {
+    class MusicLib {
         private DataSet musicDataSet;
 
         public const string XML_MUSICFILE = "music.xml";
@@ -19,40 +18,32 @@ namespace hTunes
         /// <summary>
         /// The list of all song IDs in sorted order
         /// </summary>
-        public string[] SongIds
-        {
-            get
-            {
+        public string[] SongIds {
+            get {
                 return GetList("song", "id");
             }
         }
 
-        public string[] Playlists
-        {
-            get
-            {
+        public string[] Playlists {
+            get {
                 return GetList("playlist", "name");
             }
         }
 
-        public DataTable Songs
-        {
-            get
-            {
+        public DataTable Songs {
+            get {
                 return musicDataSet.Tables["song"];
             }
         }
 
-        private string[] GetList(string tableName, string colName)
-        {
+        private string[] GetList(string tableName, string colName) {
             var items = from row in musicDataSet.Tables[tableName].AsEnumerable()
                         orderby row[colName]
                         select row[colName].ToString();
             return items.ToArray();
         }
 
-        public MusicLib()
-        {
+        public MusicLib() {
             musicDataSet = new DataSet();
             musicDataSet.ReadXmlSchema(XSD_MUSICFILE);
             musicDataSet.ReadXml(XML_MUSICFILE);
@@ -64,8 +55,7 @@ namespace hTunes
         /// </summary>
         /// <param name="s">Song to add</param>
         /// <returns>The song's ID</returns>
-        public int AddSong(Song s)
-        {
+        public int AddSong(Song s) {
             DataTable table = musicDataSet.Tables["song"];
             DataRow row = table.NewRow();
 
@@ -83,19 +73,17 @@ namespace hTunes
             return s.Id;
         }
 
-		/// <summary>
+        /// <summary>
         /// Add a song pointed to by the filename.  Returns the newly added song.
         /// </summary>
         /// <param name="filename">MP3 filename</param>
         /// <returns>Song created from the MP3</returns>
-        public Song AddSong(string filename)
-        {           
+        public Song AddSong(string filename) {
             // PM> Install-Package taglib
             // http://stackoverflow.com/questions/1750464/how-to-read-and-write-id3-tags-to-an-mp3-in-c
             TagLib.File file = TagLib.File.Create(filename);
 
-            Song s = new Song
-            {
+            Song s = new Song {
                 Title = file.Tag.Title,
                 Artist = file.Tag.AlbumArtists[0],
                 Album = file.Tag.Album,
@@ -104,25 +92,24 @@ namespace hTunes
                 Filename = filename
             };
 
-            GetSongData(s);
+            // TODO fix function call
+            // GetSongData(s);
 
             AddSong(s);
             return s;
         }
 
-		
+
         /// <summary>
         /// Return a Song for the given song ID. Returns null if the song was not found.
         /// </summary>
         /// <param name="songId">ID of song to search for</param>
         /// <returns>The song matching the songId or null if the song wasn't found</returns>
-        public Song GetSong(int songId)
-        {
+        public Song GetSong(int songId) {
             DataTable table = musicDataSet.Tables["song"];
 
             // Only one row should be selected
-            foreach (DataRow row in table.Select("id=" + songId))
-            {
+            foreach (DataRow row in table.Select("id=" + songId)) {
                 Song song = new Song();
                 song.Id = songId;
                 song.Title = row["title"].ToString();
@@ -148,13 +135,11 @@ namespace hTunes
         /// <param name="songId">Song ID to search for</param>
         /// <param name="song">Song data that has possibly changed</param>
         /// <returns>true if the song with the song ID was found, false otherwise</returns>
-        public bool UpdateSong(int songId, Song song)
-        {
+        public bool UpdateSong(int songId, Song song) {
             DataTable table = musicDataSet.Tables["song"];
 
             // Only one row should be selected
-            foreach (DataRow row in table.Select("id=" + songId))
-            {
+            foreach (DataRow row in table.Select("id=" + songId)) {
                 row["title"] = song.Title;
                 row["artist"] = song.Artist;
                 row["album"] = song.Album;
@@ -175,8 +160,7 @@ namespace hTunes
         /// </summary>
         /// <param name="songId">Song ID to delete</param>
         /// <returns>true if the song ID was found and deleted, false otherwise</returns>
-        public bool DeleteSong(int songId)
-        {
+        public bool DeleteSong(int songId) {
             // Search the primary key for the selected song and delete it from 
             // the song table
             DataTable table = musicDataSet.Tables["song"];
@@ -206,8 +190,7 @@ namespace hTunes
         /// <summary>
         /// Save the song database to file.
         /// </summary>
-        public void Save()
-        {
+        public void Save() {
             // Save music.xml 
             Console.WriteLine("Saving " + XML_MUSICFILE);
             musicDataSet.WriteXml(XML_MUSICFILE);
@@ -216,17 +199,13 @@ namespace hTunes
         /// <summary>
         /// Debug information displaying all the table data in the console.
         /// </summary>
-        public void PrintAllTables()
-        {
-            foreach (DataTable table in musicDataSet.Tables)
-            {
+        public void PrintAllTables() {
+            foreach (DataTable table in musicDataSet.Tables) {
                 Console.WriteLine("Table name = " + table.TableName);
-                foreach (DataRow row in table.Rows)
-                {
+                foreach (DataRow row in table.Rows) {
                     Console.WriteLine("Row:");
                     int i = 0;
-                    foreach (Object item in row.ItemArray)
-                    {
+                    foreach (Object item in row.ItemArray) {
                         Console.WriteLine(" " + table.Columns[i].Caption + "=" + item);
                         i++;
                     }
@@ -242,19 +221,15 @@ namespace hTunes
         /// </summary>
         /// <param name="playlist">Name of the playlist</param>
         /// <returns>True if the playlist was successfully added</returns>
-        public bool AddPlaylist(string playlist)
-        {
+        public bool AddPlaylist(string playlist) {
             Console.WriteLine("AddPlaylist: " + playlist);
             DataTable table = musicDataSet.Tables["playlist"];
             DataRow row = table.NewRow();
             row["name"] = playlist;
 
-            try
-            {
+            try {
                 table.Rows.Add(row);
-            }
-            catch (Exception)
-            {
+            } catch (Exception) {
                 // Probably a playlist with the same name was being added
                 return false;
             }
@@ -271,11 +246,10 @@ namespace hTunes
         /// <param name="oldPlaylistName">Name of playlist to change</param>
         /// <param name="newPlaylistName">New name for playlist</param>
         /// <returns>True if the playlist's name was successfully changed</returns>
-        public bool RenamePlaylist(string oldPlaylistName, string newPlaylistName)
-        {
+        public bool RenamePlaylist(string oldPlaylistName, string newPlaylistName) {
             Console.WriteLine("RenamePlaylist: " + oldPlaylistName + " to " + newPlaylistName);
 
-			newPlaylistName = newPlaylistName.Trim();
+            newPlaylistName = newPlaylistName.Trim();
             if (newPlaylistName == "" || oldPlaylistName == newPlaylistName)
                 return false;
             // Update playlist name in playlist and playlist_song tables
@@ -300,8 +274,7 @@ namespace hTunes
         /// </summary>
         /// <param name="playlist">Playlist to search for</param>
         /// <returns>True if the playlist exists</returns>
-        public bool PlaylistExists(string playlist)
-        {
+        public bool PlaylistExists(string playlist) {
             DataTable table = musicDataSet.Tables["playlist"];
             DataRow row = table.Rows.Find(playlist);
             return row != null;
@@ -313,8 +286,7 @@ namespace hTunes
         /// </summary>
         /// <param name="playlist">Playlist to delete</param>
         /// <returns>True if the playlist was found and deleted</returns>
-        public bool DeletePlaylist(string playlist)
-        {
+        public bool DeletePlaylist(string playlist) {
             // Search the primary key for this playlist and delete it from the playlist table
             DataTable table = musicDataSet.Tables["playlist"];
             DataRow row = table.Rows.Find(playlist);
@@ -341,8 +313,7 @@ namespace hTunes
         /// </summary>
         /// <param name="songId"></param>
         /// <param name="playlist"></param>
-        public void AddSongToPlaylist(int songId, string playlist)
-        {
+        public void AddSongToPlaylist(int songId, string playlist) {
             // Find the last position in the playlist
             int pos = GetLastPosition(playlist);
 
@@ -362,8 +333,7 @@ namespace hTunes
         /// </summary>
         /// <param name="playlist"></param>
         /// <returns></returns>
-        private int GetLastPosition(string playlist)
-        {
+        private int GetLastPosition(string playlist) {
             Console.WriteLine("playlist=" + playlist);
             DataTable table = musicDataSet.Tables["playlist_song"];
             var positions = from row in table.AsEnumerable()
@@ -378,8 +348,7 @@ namespace hTunes
         /// <param name="position"></param>
         /// <param name="songId"></param>
         /// <param name="playlist"></param>
-        public void RemoveSongFromPlaylist(int position, int songId, string playlist)
-        {
+        public void RemoveSongFromPlaylist(int position, int songId, string playlist) {
             Console.WriteLine("RemoveSongFromPlaylist: id=" + songId + ", pos=" + position +
                 ", playlist=" + playlist);
 
@@ -396,8 +365,7 @@ namespace hTunes
             // Decrement position by 1 for each song in this playlist that is positioned after
             // this one
             table = musicDataSet.Tables["playlist_song"];
-            foreach (DataRow r in table.Rows)
-            {
+            foreach (DataRow r in table.Rows) {
                 int pos = Convert.ToInt32(r["position"]);
                 if ((string)r["playlist_name"] == playlist && pos > position)
                     r["position"] = pos - 1;
@@ -411,8 +379,7 @@ namespace hTunes
         /// </summary>
         /// <param name="playlist"></param>
         /// <returns></returns>
-        public DataTable SongsForPlaylist(string playlist)
-        {
+        public DataTable SongsForPlaylist(string playlist) {
             // Create a table with song attributes and position
             DataTable table = new DataTable();
             table.Columns.Add("id");
@@ -428,8 +395,7 @@ namespace hTunes
                              on r1["song_id"] equals r2["id"]
                         where (string)r1["playlist_name"] == playlist
                         orderby r1["position"]
-                        select new
-                        {
+                        select new {
                             Id = r2["id"],
                             Position = r1["position"],
                             Title = r2["title"],
@@ -439,8 +405,7 @@ namespace hTunes
                         };
 
             Console.WriteLine("Songs for playlist " + playlist + ":");
-            foreach (var s in songs)
-            {
+            foreach (var s in songs) {
                 Console.WriteLine(s.ToString());
                 DataRow newRow = table.NewRow();
                 newRow["id"] = s.Id;
